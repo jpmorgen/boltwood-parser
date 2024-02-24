ef619ba6430544f52e234dc0ed4a71b8735d4ce4
kinderhead <67707412+kinderhead@users.noreply.github.com>
2024-02-24 16:25:13 -0500

    Does not preprocess entries by default

--- src/boltwood_parser/collection.py
+++ src/boltwood_parser/collection.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import math
+from typing import cast
 from .entry import WeatherEntry
 
 
@@ -8,10 +9,10 @@ class EntryCollection:
     A class for parsing, storing, and processing multiple weather entries.
     """
 
-    entries: list[WeatherEntry] = []
+    entries: list[WeatherEntry | str] = []
     """Entries"""
 
-    def __init__(self, data: str, sort: bool = False) -> None:
+    def __init__(self, data: str, preprocess: bool = False, sort: bool = False) -> None:
         """
         Start parsing through weather entries that are separated by new lines.
         If `sort` is `False`, then the data will not be sorted. In order for
@@ -20,22 +21,39 @@ class EntryCollection:
         Parameters
         ----------
         data : str
-            Raw weather data. Could be read from a file.
+            Raw weather data. Could be read from a file
+        preprocess : bool, optional
+            Preprocesses weather data, by default `False`
         sort : bool, optional
-            Sort the data by time, by default `False`
+            Sort the data by time, by default `False`. Sets `preprocess` to `True`.
         """
+        if sort: preprocess = True
+        
         lines = data.splitlines()
-        for i in lines:
-            if i.strip() == "":
-                continue
-            self.entries.append(self.process_entry(i))
+        
+        if preprocess:
+            for i in lines:
+                if i.strip() == "":
+                    continue
+                try:
+                    self.entries.append(self.process_entry(i))
+                except Exception as e:
+                    print(f"ERROR: received {e} when parsing {i}")
+        else:
+            self.entries = cast(list[WeatherEntry | str], lines)
 
         if sort:
-            self.entries.sort(key=lambda x: x.time.timestamp())
+            def predicate(x: WeatherEntry | str):
+                if type(x) is WeatherEntry:
+                    return x.time.timestamp()
+                else:
+                    return 0
+            
+            self.entries.sort(key=predicate)
 
     def process_entry(self, line: str) -> WeatherEntry:
         """
-        Process and entry before storing it. Override this to use custom
+        Process an entry before storing it. Override this to use custom
         weather entry objects.
 
         Parameters
@@ -49,6 +67,30 @@ class EntryCollection:
             Processed entry
         """
         return WeatherEntry(line)
+    
+    def parse(self, index: int) -> WeatherEntry:
+        """
+        Parse a line and return the entry, or return the entry if already parsed.
+
+        Parameters
+        ----------
+        index : int
+            Index
+
+        Returns
+        -------
+        WeatherEntry
+            Entry
+        """
+        entry = self.entries[index]
+        if type(entry) is str:
+            processed = self.process_entry(entry)
+            self.entries[index] = processed
+            return processed
+        elif type(entry) is WeatherEntry:
+            return entry
+        else:
+            raise TypeError(f"Entry {entry} is neither a string nor a WeatherEntry.")
     
     def find(self, time: datetime) -> WeatherEntry:
         """
@@ -64,15 +106,14 @@ class EntryCollection:
         WeatherEntry
             Weather entry closest to `time`
         """
-        if time < self.entries[0].time:
-            return self.entries[0]
-        elif time > self.entries[-1].time:
-            return self.entries[-1]
+        if time < self.parse(0).time:
+            return self.parse(0)
+        elif time > self.parse(-1).time:
+            return self.parse(-1)
         
-        return self.__find(time)
+        return self._find(time)
         
-        
-    def __find(self, time: datetime, _low: int = 0, _high: int = -1) -> WeatherEntry:
+    def _find(self, time: datetime, _low: int = 0, _high: int = -1) -> WeatherEntry:
         """
         Internal method for searching. Does not account for first or last entry.
         """
@@ -80,15 +121,15 @@ class EntryCollection:
             _high = len(self.entries) - 1
             
         if _high == _low:
-            found = self.entries[_high]
-            next_to_found = self.entries[_high - 1]
+            found = self.parse(_high)
+            next_to_found = self.parse(_high - 1)
             if abs(time.timestamp() - found.time.timestamp()) < abs(time.timestamp() - next_to_found.time.timestamp()):
                 return found
             else:
                 return next_to_found
         
         mid = math.floor(_low + ((_high - _low) / 2))
-        if self.entries[mid].time > time:
-            return self.__find(time, _low, mid)
+        if self.parse(mid).time > time:
+            return self._find(time, _low, mid)
         else:
-            return self.__find(time, mid + 1, _high)
+            return self._find(time, mid + 1, _high)
